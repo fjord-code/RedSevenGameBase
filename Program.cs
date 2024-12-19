e4d490d219450a0ed4ca8d2496c8e7905e8484c9
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2024-12-19 22:48:14 +0300

    Initial commit
    An initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RedSevenGameBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
