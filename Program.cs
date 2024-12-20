0ea18f07a0a90e633a43a97dc9f6ff1a7aadccf7
Anatoli Dobshikov <anatoliy.dobshikov@gmail.com>
2024-12-21 00:13:05 +0300

    Implemented logic
    Finished logic implementation.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RedSevenGameBase
+﻿using System.Runtime.CompilerServices;
+
+namespace RedSevenGameBase
 {
     enum Numbers
     {
@@ -47,35 +49,137 @@
         private static int GetColorsValue(Colors color)
         {
             if (!_colorsSortOrder.ContainsKey(color))
-                throw new InvalidOperationException($"The value is not defined for the color {nameof(color)}");
+                throw new InvalidOperationException($"The value is not defined for the color {color}");
 
             return _colorsSortOrder[color];
         }
     }
 
-    struct Card(Colors color, Numbers number)
+    struct Card(Colors color, Numbers number) : IComparable<Card>
     {    
         public Color Color { get; } = new Color(color);
-        public Numbers Number { get; } = number;     
+        public Numbers Number { get; } = number;             
 
-        private class SortCardAscendingHelper : IComparer<Card>
+        public int CompareTo(Card other)
         {
-            int IComparer<Card>.Compare(Card x, Card y)
-            {
-                int numberComparisson = x.Number.CompareTo(y.Number);
+            int numberComparisson = this.Number.CompareTo(other.Number);
 
-                return numberComparisson != 0 ? numberComparisson : x.Color.Value.CompareTo(y.Color.Value);
+            return numberComparisson != 0 ? numberComparisson : this.Color.Value.CompareTo(other.Color.Value);
+        }
+
+        public override string ToString() => $"{(byte)Number} {Color.Name}";
+    }
+
+    struct CardCombination
+    {
+        private List<Card> _cards;
+        private Card? _highestCard;
+
+        public Card HighestCard
+        {
+            get
+            {
+                if (_highestCard.HasValue) 
+                    return _highestCard.Value;
+
+                throw new InvalidOperationException("There is no highest card in the card combination.");
             }
         }
 
-        public static IComparer<Card> SortCardAscending => new SortCardAscendingHelper();
+        public CardCombination()
+        {
+            _cards = new List<Card>();
+            _highestCard = null;
+        }
+
+        public void Add(Card card)
+        {
+            _cards.Add(card);
+
+            if (!_highestCard.HasValue || _highestCard.Value.CompareTo(card) < 0)
+                _highestCard = card;
+        }
     }
 
     internal class Program
     {
+        private static HashSet<Card> _usedCards = new HashSet<Card>();
+
+        static int GetCombinationLenghtFromConsole()
+        {
+            Console.Write("Input the combination length: ");
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        static Card GetCardFromConsole()
+        {
+            Console.Write("Input a card (value and color separated by a space): ");
+
+            var rawInput = Console.ReadLine();
+            var rawValues = rawInput.Split(' ');
+            var value = int.Parse(rawValues[0]);
+            var color = rawValues[1][0];
+
+            return new Card(
+                (Colors)Enum.ToObject(typeof(Colors), color),
+                (Numbers)Enum.ToObject(typeof(Numbers), value)
+            );
+        }
+
+        static void AddCardToCombination(ref CardCombination cardCombination, Card card)
+        {
+            if (_usedCards.Contains(card))
+                throw new InvalidOperationException("The card is already in use.");
+
+            _usedCards.Add(card);
+            cardCombination.Add(card);
+        }
+
+        static void AddCardFromConsoleToCombination(ref CardCombination cardCombination)
+        {
+            var card = GetCardFromConsole();
+            AddCardToCombination(ref cardCombination, card);
+        }
+
+        static CardCombination CreateCardCombinationFromConsole(int combinationLength)
+        {
+            var cardCombination = new CardCombination();
+
+            for (var i = 0; i < combinationLength; i++)
+                AddCardFromConsoleToCombination(ref cardCombination);
+
+            return cardCombination;
+        }
+
+        static CardCombination CreateCardCombinationFromConsole()
+        {
+            var combinationLength = GetCombinationLenghtFromConsole();
+            return CreateCardCombinationFromConsole(combinationLength);
+        }
+
+        static void StartGame()
+        {
+            _usedCards.Clear();
+
+            Console.WriteLine("Input the first combination.");
+            var firstCombination = CreateCardCombinationFromConsole();
+
+            Console.WriteLine("Input the second combination.");
+            var secondCombination = CreateCardCombinationFromConsole();
+
+            var highestCardsComparisson = firstCombination.HighestCard.CompareTo(secondCombination.HighestCard);
+            if (highestCardsComparisson > 0)
+                Console.WriteLine($"The first combination wins.\n{firstCombination.HighestCard}");
+            else if (highestCardsComparisson == 0)
+                Console.WriteLine("The game is tied.");
+            else
+                Console.WriteLine($"The second combination wins.\n{secondCombination.HighestCard}");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StartGame();
         }
     }
 }
